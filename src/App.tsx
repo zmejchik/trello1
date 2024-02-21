71caeb4140ea43b54db132b447100e4d7f5f0788
zmejchik <zmejchik@gmail.com>
2024-02-21 15:08:35 +0200

    husky finish

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App(): JSX.Element {
-  let a = "ffff" -  
   return (
     <div className="App">
       <header className="App-header">
