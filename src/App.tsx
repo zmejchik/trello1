0f90170d38bdb94ae9e9c34f3337317accee3852
zmejchik <zmejchik@gmail.com>
2024-03-02 14:24:07 +0200

    edit route

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import { Home } from './Home';
 function App(): JSX.Element {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/board" element={<Board />} />
+      <Route path="/trello" element={<Home />} />
+      <Route path="/trello/board" element={<Board />} />
     </Routes>
   );
 }
