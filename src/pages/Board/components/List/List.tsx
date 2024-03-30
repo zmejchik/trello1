ec3f84ffcf03a37bb01c82730a7241351dfb49af
zmejchik <zmejchik@gmail.com>
2024-03-30 14:15:59 +0200

    edit list name

--- src/pages/Board/components/List/List.tsx
+++ src/pages/Board/components/List/List.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FaSquarePlus } from 'react-icons/fa6';
+import { FaClipboard } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
 import s from './list.module.scss';
 import Button from '../Button/Button';
@@ -8,11 +9,16 @@ import { Card } from '../Card/Card';
 import { IList } from '../../../../common/interfaces/IList';
 import { Modal } from '../../../../common/components/Modal/Modal';
 import { ICard } from '../../../../common/interfaces/ICard';
+import { isValidBoardName as isValidListName } from '../../../../common/components/CreateBoardLogic/CreateBoard';
 
-function List({ id, title, cards: cardsArray }: IList): JSX.Element {
+function List({ id, title: titleList, cards: cardsArray }: IList): JSX.Element {
   const [value, setValue] = useState('');
   const [isModal, setModal] = useState(false);
   const [cards, setcards] = useState(cardsArray);
+  const [listName, setListName] = useState(titleList);
+  const [isEditingNameList, setIsEditingNameList] = useState(false);
+  const [inputValueNameList, setInputValueNameList] = useState(listName);
+
   const onClose = (): void => setModal(!isModal);
   const { boardId } = useParams();
 
@@ -37,10 +43,43 @@ function List({ id, title, cards: cardsArray }: IList): JSX.Element {
     }
   };
 
+  const editNameList = async (title: string): Promise<void> => {
+    if (isValidListName(title)) {
+      try {
+        await api.put(`/board/${boardId}/list/${id}`, { title });
+        const data: { lists: IList[] } = await api.get(`/board/${boardId}`);
+        const newListName = data.lists.find((list) => list.id === id)?.title || '';
+        setListName(newListName);
+        setIsEditingNameList(false);
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Error editing board name:', error);
+      }
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('Incorrect list name');
+    }
+  };
+
   return (
     <>
       <div className={s.list}>
-        <h2 className={s.list_title}>{title}</h2>
+        {isEditingNameList ? (
+          <h2 className={s.listH2}>
+            <FaClipboard className={s.listIcon} />
+            <input
+              className={s.list_inputForEditionNameList}
+              value={inputValueNameList}
+              onChange={(event): void => setInputValueNameList(event.target.value)}
+              onBlur={(): Promise<void> => editNameList(inputValueNameList)}
+            />
+          </h2>
+        ) : (
+          <h2 className={s.list_title} onClick={(): void => setIsEditingNameList(true)}>
+            {listName}
+          </h2>
+        )}
+
         <div className={s.list_body}>
           {cards.map(({ id: cardId, title: titleCard }: ICard) => (
             <Card key={cardId} id={cardId} title={titleCard} />
