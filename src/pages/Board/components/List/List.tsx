a6ce106899b1c0175f3ca31b15ec3a58564f61dd
zmejchik <zmejchik@gmail.com>
2024-06-29 15:54:38 +0300

    dnd

--- src/pages/Board/components/List/List.tsx
+++ src/pages/Board/components/List/List.tsx
@@ -21,7 +21,7 @@ function List({ id, title: titleList, cards: cardsArray, setRenderList }: IList)
   const [listName, setListName] = useState(titleList);
   const [isEditingNameList, setIsEditingNameList] = useState(false);
   const [inputValueNameList, setInputValueNameList] = useState(listName);
-  const [, setIsDraggingCard] = useState(false);
+  const [isDragging, setIsDragging] = useState(false);
   const draggingCardId = useRef<number>(-1);
 
   const onClose = (): void => setModal(false);
@@ -34,13 +34,19 @@ function List({ id, title: titleList, cards: cardsArray, setRenderList }: IList)
     }
   }, [isEditingNameList]);
 
+  useEffect(() => {
+    if (cards.length === 0) {
+      setCards([{ id: -1, title: '', list_id: id, position: 1 }]);
+    }
+  }, [cards.length, id]);
+
   function dragOverHandler(event: React.DragEvent<HTMLDivElement>): void {
     event.preventDefault();
-    setIsDraggingCard(true);
+    setIsDragging(true);
   }
 
   function dragLeaveHandler(): void {
-    setIsDraggingCard(false);
+    setIsDragging(false);
     const newCards = cards.filter((card) => card.id !== -1);
     setTimeout(() => {
       newCards.forEach((card, index) => {
@@ -74,7 +80,7 @@ function List({ id, title: titleList, cards: cardsArray, setRenderList }: IList)
             Object.assign(card, newCard);
           });
           const slotCard: ICard[] = cards.filter((card) => card.id === -1);
-          draggedCard.position = slotCard[0].position;
+          draggedCard.position = slotCard.length > 0 ? slotCard[0].position : cards.length + 1;
           cards.forEach((card, index) => {
             if (card.position === draggedCard?.position) {
               cards[index] = draggedCard as ICard;
@@ -86,7 +92,7 @@ function List({ id, title: titleList, cards: cardsArray, setRenderList }: IList)
           });
           await api.post(`/board/${boardId}/card/`, draggedCard);
 
-          setIsDraggingCard(false);
+          setIsDragging(false);
           await updateCardList(boardId, id, setCards);
           setCards(cards);
         }
@@ -100,17 +106,18 @@ function List({ id, title: titleList, cards: cardsArray, setRenderList }: IList)
       }
     }
     setRenderList(true);
-    /* field for coding  */
 
     const dataFromServer: { lists: IList[] } = await api.get(`/board/${boardId}`);
     const listFromServer = dataFromServer.lists.find((list) => list.id === id);
     const cardFromServer = listFromServer?.cards.find((card) => card.title === draggedCard?.title);
 
     const updateDataOnServer = cards.map((card) => {
-      if (card.title === draggedCard?.title && card.id !== undefined && cardFromServer?.id) {
-        card.id = cardFromServer?.id;
-      }
-      return { id: card.id, position: card.position, list_id: id };
+      const updatedCard =
+        card.title === draggedCard?.title && card.id !== undefined && cardFromServer?.id
+          ? { ...card, id: cardFromServer.id }
+          : card;
+
+      return { id: updatedCard.id, position: updatedCard.position, list_id: id };
     });
 
     await api.put(`/board/${boardId}/card`, updateDataOnServer);
@@ -124,19 +131,20 @@ function List({ id, title: titleList, cards: cardsArray, setRenderList }: IList)
     const isBelowHalf = mousePos > element.offsetHeight / 2;
     const newCards = [...cards];
     const cardIndex = newCards.findIndex((card) => card.id === cardId);
-    // Remove any existing slot cards
     const existingSlotIndex = newCards.findIndex((card) => card.id === -1);
     if (existingSlotIndex !== -1) {
       newCards.splice(existingSlotIndex, 1);
     }
     if (cardIndex !== -1 && newCards[cardIndex] !== undefined) {
-      // Create a new slot card
       const slotCard = { id: -1, title: '', list_id: id, position: newCards[cardIndex].position };
-      if (isBelowHalf) {
-        newCards.splice(cardIndex + 1, 0, slotCard);
-      } else {
-        newCards.splice(cardIndex - 1, 0, slotCard);
+      if (newCards.length > 0) {
+        if (isBelowHalf) {
+          newCards.splice(cardIndex + 1, 0, slotCard);
+        } else {
+          newCards.splice(cardIndex, 0, slotCard);
+        }
       }
+
       newCards.forEach((card, index) => {
         const newCard = { ...card, position: index + 1 };
         Object.assign(card, newCard);
@@ -170,57 +178,61 @@ function List({ id, title: titleList, cards: cardsArray, setRenderList }: IList)
 
   return (
     <>
-      <div className={s.list} onDragOver={dragOverHandler} onDragLeave={dragLeaveHandler} onDrop={dropHandler}>
-        {isEditingNameList ? (
-          <h2 className={s.listH2}>
-            <FaClipboard className={s.listIcon} />
-            <input
-              className={s.list_inputForEditionNameList}
-              value={inputValueNameList}
-              onChange={(event): void => setInputValueNameList(event.target.value)}
-              onBlur={(): Promise<void> =>
-                boardId
-                  ? editNameList(boardId, id, inputValueNameList, setListName, setIsEditingNameList)
-                  : Promise.resolve()
-              }
-              onKeyDown={(ev): void => {
-                if (ev.key === 'Enter' && boardId) {
-                  editNameList(boardId, id, inputValueNameList, setListName, setIsEditingNameList);
+      {cards.length > 0 && (
+        <div className={s.list} onDragOver={dragOverHandler} onDragLeave={dragLeaveHandler} onDrop={dropHandler}>
+          {isEditingNameList ? (
+            <h2 className={s.listH2}>
+              <FaClipboard className={s.listIcon} />
+              <input
+                className={s.list_inputForEditionNameList}
+                value={inputValueNameList}
+                onChange={(event): void => setInputValueNameList(event.target.value)}
+                onBlur={(): Promise<void> =>
+                  boardId
+                    ? editNameList(boardId, id, inputValueNameList, setListName, setIsEditingNameList)
+                    : Promise.resolve()
                 }
-              }}
-              ref={inputRef}
-            />
-          </h2>
-        ) : (
-          <h2
-            className={s.list_title}
-            onDragOver={dragOverHandler}
-            onDrop={dropHandler}
-            onClick={(): void => setIsEditingNameList(true)}
-          >
-            {listName}
-          </h2>
-        )}
+                onKeyDown={(ev): void => {
+                  if (ev.key === 'Enter' && boardId) {
+                    editNameList(boardId, id, inputValueNameList, setListName, setIsEditingNameList);
+                  }
+                }}
+                ref={inputRef}
+              />
+            </h2>
+          ) : (
+            <h2
+              className={s.list_title}
+              onDragOver={dragOverHandler}
+              onDrop={dropHandler}
+              onClick={(): void => setIsEditingNameList(true)}
+            >
+              {listName}
+            </h2>
+          )}
 
-        <div className={s.list_body}>
-          {cards.map(({ id: cardId, title: titleCard }: ICard) => (
-            <Card
-              key={cardId}
-              id={cardId}
-              title={titleCard}
-              list_id={id}
-              updateCardList={(): Promise<void> =>
-                boardId ? updateCardList(boardId, id, setCards) : Promise.resolve()
-              }
-              onDragStart={(event): void => dragStartHandler(event, cardId)}
-              onDragEnter={(event): void => dragEnterHandler(event, cardId)}
-              classSlot={cardId === -1 ? 'slotCard' : ''}
-            />
-          ))}
+          <div className={s.list_body}>
+            {cards
+              .filter(({ id: cardId }) => !(cardId === -1 && !isDragging))
+              .map(({ id: cardId, title: titleCard }: ICard) => (
+                <Card
+                  key={cardId}
+                  id={cardId}
+                  title={titleCard}
+                  list_id={id}
+                  updateCardList={(): Promise<void> =>
+                    boardId ? updateCardList(boardId, id, setCards) : Promise.resolve()
+                  }
+                  onDragStart={(event): void => dragStartHandler(event, cardId)}
+                  onDragEnter={(event): void => dragEnterHandler(event, cardId)}
+                  classSlot={cardId === -1 && cards.length > 1 ? 'slotCard' : ''}
+                />
+              ))}
+          </div>
+
+          <Button icon={<FaSquarePlus />} caption="Створити картку" onClick={(): void => setModal(true)} />
         </div>
-
-        <Button icon={<FaSquarePlus />} caption="Створити картку" onClick={(): void => setModal(true)} />
-      </div>
+      )}
 
       <Modal
         visible={isModal}
